167976c98fccae03e668923b7d107095a309afef
John Howard <fatred+ccmeapp@fatred.co.uk>
2015-12-23 19:22:37 +0000

    Added sqlite support for the simple demo

--- simple-query.py
+++ simple-query.py
@@ -11,6 +11,8 @@ from config import Config
 import getpass
 import paramiko
 import time
+import re
+import sqlite3
 
 # basics
 # use basic config file mgmt
@@ -81,9 +83,59 @@ try:
     """
         regexing the handsets
         ephone num
+            cur.execute('INSERT OR REPLACE INTO handsets (ephone_id, desc, "mac-address", "ephone-template", type, button) VALUES (999, "horse", "1111.2222.3333", 1, 7911, 12)')
             print "ephone num %s" % re.search(re.compile(ur'^ephone\s*(\d*)$'), handset.ioscfg[0]).group(1)
-
+            print "mac addr %s" % re.search(re.compile(ur'\smac-address\s*(\w{4}\.\w{4}\.\w{4})'), str(handsets[20].ioscfg)).group(1)
     """
+    
+    print "[*] Connecting to Database ccme-mgmt"
+    db = sqlite3.connect('ccme-mgmt.sqlite')
+    cur = db.cursor()
+    
+    print "[*] Importing handsets into Database"
+    for handset in handsets:
+        # ephone number
+        ephone_id = re.search(re.compile(ur'ephone\s*(\d*)'), str(handset.ioscfg))
+        if ephone_id:
+            cur.execute('INSERT OR REPLACE INTO handsets (ephone_id) VALUES (?)', ephone_id.group(1)) 
+        else:
+            print "[E] No ephone number found in record"
+            break
+        # description 
+        desc = re.search(re.compile(ur'\s+description\s+([^\']+)'), str(handset.ioscfg))
+        if desc:
+            cur.execute('INSERT OR REPLACE INTO handsets (ephone_id,desc) VALUES (?,?)', (ephone_id.group(1), desc.group(1)))
+        else:
+            cur.execute('INSERT OR REPLACE INTO handsets (ephone_id,desc) VALUES (?,?)', (ephone_id.group(1), "none"))
+        # mac address
+        mac_addr = re.search(re.compile(ur'\smac-address\s*(\w{4}\.\w{4}\.\w{4})'), str(handset.ioscfg))
+        if mac_addr:
+            cur.execute('INSERT OR REPLACE INTO handsets (ephone_id,"mac-address") VALUES (?,?)', (ephone_id.group(1)), mac_addr.group(1))
+        else:
+            print "[E] No MAC Address on ephone %s" % ephone_id.group(1)
+            break
+        # type
+        type_num = re.search(re.compile(ur'\s+type\s+([^\'])'), str(handset.ioscfg))
+        if type_num:
+            cur.execute('INSERT OR REPLACE INTO handsets (ephone_id,type) VALUES (?,?)', (ephone_id.group(1), type_num.group(1)))
+        else:
+            print "[W] No Type provided for ephone %s, default to 7911" % ephone_id.group(1)
+        # button
+        button_num = re.search(re.compile(ur'\s+button\s+\d\:([^\']+)'), str(handset.ioscfg))
+        if button_num:
+            cur.execute('INSERT OR REPLACE INTO handsets (ephone_id,button) VALUES (?,?)', (ephone_id.group(1), button_num.group(1)))
+        else:
+            print "[W] No Dirnum button assigned to ephone %s" % ephone_id.group(1)
+        # ephone-templ
+        templ_num = re.search(re.compile(ur'\s+ephone-template\s+([^\']+)'), str(handset.ioscfg))
+        if templ_num:
+            cur.execute('INSERT OR REPLACE INTO handsets (ephone_id,"ephone-template") VALUES (?,?)', (ephone_id.group(1), templ_num.group(1)))
+        else:
+            print "[W] No ephone template on ephone %s" % ephone_id.group(1)
+        
+        # be shouty
+        print "[*] Added ephone %s|%s to Database!" % (ephone_id.group(1), mac_addr.group(1))
+
     # cleanup
     session.close()
     client.close()
