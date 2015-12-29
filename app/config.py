976767981632417b539899f433bc9f551f8cf9d0
John Howard <fatred+ccmeapp@gmail.com>
2015-12-29 17:47:13 +0000

    Addedd Horse Support

--- app/config.py
+++ app/config.py
@@ -3,3 +3,4 @@ class Configuration(object):
     APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///%s/ccme-mgmt.db' % APPLICATION_DIR
+    HORSE = True
